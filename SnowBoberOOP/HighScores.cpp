7d3c4d28bde2daad65c74fa9c69814686edbbad8
baltek99 <baltek99@gmail.com>
2022-04-18 14:50:35 +0200

    highscores feature

---
+++ SnowBoberOOP/HighScores.cpp
@@ -0,0 +1,17 @@
+#include "HighScores.h"
+
+HighScores::HighScores() : maxNumberOfResults(10), scores(std::vector<ResultBind>()) {}
+
+std::vector<ResultBind> HighScores::getScores() const {
+	return scores;
+}
+
+void HighScores::addResult(const std::string name, int score) {
+	scores.push_back(ResultBind(name, score));
+
+	//sort
+
+	if (scores.size() > maxNumberOfResults) {
+		scores.
+	}
+}
---
+++ SnowBoberOOP/HighScores.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include "ResultBind.h"
+
+class HighScores
+{
+private:
+	// zamieniæ na deque
+	std::vector<ResultBind> scores;
+	const int maxNumberOfResults;
+
+public :
+	HighScores();
+
+	std::vector<ResultBind> getScores() const;
+
+	void addResult(const std::string name, int score);
+};
+
