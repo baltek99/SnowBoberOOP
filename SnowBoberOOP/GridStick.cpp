8c62352d94830a679a0b91464e7d5cf7538c9329
baltek99 <baltek99@gmail.com>
2022-07-01 16:33:39 +0200

    broken obstacles

--- SnowBoberOOP/GridStick.cpp
+++ SnowBoberOOP/GridStick.cpp
@@ -1,6 +1,5 @@
 #include "GridStick.h"
 #include "Player.h"
-#include <iostream>
 
 GridStick::GridStick(const Position &position_, int speed_) :
 	Obstacle(position_, Visual(TexturesManager::gridStick, ConstValues::GRID_WIDTH, ConstValues::GRID_HEIGHT), speed_, ObstacleType::GRID_STICK) {
@@ -11,7 +10,6 @@ void GridStick::collide(ICollidable *object) {
 	Player* player = static_cast<Player*>(object);
 
 	if (player->getPlayerState() != PlayerState::CROUCH) {
-		//getVisual().setTexture(TexturesManager::gridBroken);
 		getVisual().getSprite().setScale(0, 0);
 	}
 }
